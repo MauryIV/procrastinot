1e1b42a196d743a7dfc29f75913dbb4dc62f91ef
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-04-01 21:12:06 -0600

    started routes and apis

---
+++ controllers/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/api/authController');
+
+router.post('/login', authController.login);
+
+
+router.post('/register', authController.register);
+
+
+module.exports = router;
---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+
+
+router.use
+router.use
+
+
+module.exports = router
