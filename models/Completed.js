979bf7af47520d7d37e4f203432343bfee9d2d4d
MauryIV <mauryhughesiv@gmail.com>
2024-04-01 22:00:08 -0600

    Added models and seeds.

---
+++ models/Completed.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Completed extends Model {}
+
+Completed.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    time_applied: {
+      type: DataTypes.INTEGER,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    auth_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'auth',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'completed',
+  }
+);
+
+module.exports = Completed;
---
+++ models/Working.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Working extends Model {}
+
+Working.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    time_applied: {
+      type: DataTypes.INTEGER,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    auth_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "auth",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "working",
+  }
+);
+
+module.exports = Working;
