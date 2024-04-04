e131443087c83e2550f9c7c0ad0e864854f87ed2
MauryIV <mauryhughesiv@gmail.com>
2024-04-04 11:14:12 -0600

    had some conflicts in main branch, corrected those.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,25 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-const router = require('express').Router();
-const authRoutes = require('./authRoutes');
-const todoRoutes = require('./todoRoutes');
-const workingRoutes = require('./workingRoutes');
-const completedRoutes = require('./completedRoutes');
-=======
-=======
->>>>>>> main
 const router = require("express").Router();
 const authRoutes = require("./authController");
 const todoRoutes = require("./todoController");
 const workingRoutes = require("./workingController");
 const completedRoutes = require("./completedController");
-<<<<<<< HEAD
 const TimeManager = require('als-time-manager');
 
 const timeManager = new TimeManager();
->>>>>>> Stashed changes
-=======
->>>>>>> main
 
 router.use("/auth", authRoutes);
 router.use("/todo", todoRoutes);
--- controllers/index.js
+++ controllers/index.js
@@ -1,28 +1,14 @@
 const router = require("express").Router();
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-const apiRoutes = require('./api');
-const authRoutes = require('/authRoutes');
-const todoRoutes = require('/todoRoutes');
-const workingRoutes = require('/workingRoutes');
-const completedRoutes = require('/completedRoutes');
-=======
-=======
->>>>>>> main
 const apiRoutes = require("./api");
 const landingRoutes = require("./landingRoutes");
 const authRoutes = require("./authRoutes");
 const todoRoutes = require("./todoRoutes");
 const workingRoutes = require("./workingRoutes");
 const completedRoutes = require("./completedRoutes");
-<<<<<<< HEAD
 const TimeManager = require('als-time-manager');
 
 const timeManager = new TimeManager();
->>>>>>> Stashed changes
-=======
->>>>>>> main
 
 router.use("/api", apiRoutes);
 router.use("/", landingRoutes);
