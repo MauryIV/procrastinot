5e509cbce779dbbf7cf2bd4868bd888ee49596ce
JustinHinton <justgawd7@icloud.com>
2024-04-03 21:59:38 -0600

    added java added routes

--- controllers/api/authController.js
+++ controllers/api/authController.js
@@ -2,8 +2,31 @@
 const router = require('express').Router();
 const { Auth } = require('../../models');
 
-router.post('/auth', async (req, res) => {
-
+router.post('/login', async (req, res) => {
+  try {
+    const authData = await Auth.findOne({ where: { email: req.body.email } });
+    if (!authData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+    const validPassword = await authData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = authData.id;
+      req.session.logged_in = true;
+      res.json({ auth: authData, message: 'You are now logged in!' });
+    });
+    
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/auth', async (req, res) => {
--- controllers/landingRoutes.js
+++ controllers/landingRoutes.js
@@ -2,7 +2,19 @@ const router = require('express').Router();
 const { Auth, Todo, Working, Completed } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('langingpage');
+  res.render('landingpage');
+});
+
+router.get('/auth', (req, res) => {
+  res.render('auth');
+});
+
+router.get('/create', (req, res) => {
+  res.render('createaccount');
+});
+
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard');
 });
 
 module.exports = router;
